d394d9907200829809b2264dca2e52f176a53c5b
Ji Jia Jia <kingjia90@gmail.com>
2025-03-03 13:48:02 +0100

    add cache

--- lib/Tool/MaintenanceModeHelper.php
+++ lib/Tool/MaintenanceModeHelper.php
@@ -20,6 +20,7 @@ use Doctrine\DBAL\Connection;
 use Exception;
 use InvalidArgumentException;
 use Pimcore;
+use Pimcore\Cache;
 use Pimcore\Event\SystemEvents;
 use Pimcore\Model\Tool\TmpStore;
 use Symfony\Component\EventDispatcher\GenericEvent;
@@ -76,11 +77,17 @@ class MaintenanceModeHelper implements MaintenanceModeHelperInterface
 
     protected function addEntry(string $sessionId): void
     {
+        Cache::save($sessionId, self::ENTRY_ID, lifetime: null);
         TmpStore::add(self::ENTRY_ID, $sessionId);
     }
 
     protected function getEntry(): ?string
     {
+        $entryId = Cache::load(self::ENTRY_ID);
+        if ($entryId){
+            return $entryId;
+        }
+
         try {
             $tmpStore = TmpStore::get(self::ENTRY_ID);
         } catch (Exception $e) {
@@ -94,6 +101,7 @@ class MaintenanceModeHelper implements MaintenanceModeHelperInterface
     protected function removeEntry(): void
     {
         try {
+            Cache::remove(self::ENTRY_ID);
             TmpStore::delete(self::ENTRY_ID);
         } catch (Exception $e) {
             //nothing to log as the tmp doesn't exist
